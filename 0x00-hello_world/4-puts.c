9b0e3d9c3c1e2c591a0cc3079eaf24c37669dc4d
Degxabee <ruukyyy@gmail.com>
2022-09-08 14:30:30 -0700

    bet

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
-
 /**
-* imain - print the string in the put function
+* main - print the string in the put function
 *
 * Description: using the main function
 * this program prints "Programming is like building a multilingual puzzle
